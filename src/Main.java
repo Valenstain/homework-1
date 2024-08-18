0809eafc585d6fdf0d85d048e06b5a25f9bf9143
Valenstain <svs.laboratory@gmail.com>
2024-08-18 20:34:49 +0300

    Completed BubbleSort

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,23 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+        // QUICK SORT
         //int[] arr = {10, 5, 6, 1, 4, 3, 2, 9, 8, 7};
         int[] arr = {10, 5, 6, 1, 4, 1, 2, 9, 1, 7};
         System.out.println("QUICK SORT:");
         System.out.println("Before: " + Arrays.toString(arr));
         quickSort(arr, 0, arr.length - 1);
         System.out.println("After: " + Arrays.toString(arr));
+
+        // BUBBLE SORT
+        int[] arr2 = {10, 5, 6, 1, 4, 1, 2, 9, 1, 7};
+        System.out.println("------------------------------");
+        System.out.println("BUBBLE SORT:");
+        System.out.println("Before: " + Arrays.toString(arr2));
+        bubbleSort(arr2);
+        System.out.println("After: " + Arrays.toString(arr2));
+
+
     }
 
     public static void quickSort(int[] arr, int start, int end) {
@@ -44,6 +55,20 @@ public class Main {
 
         if (end - e > 1)
             quickSort(arr, ++e, end);
+    }
+
+    public static void bubbleSort(int[] arr) {
+
+        for (int i = 0, z = arr.length - 1; i < arr.length; i++, z--) {
+            for (int s = 0; s < z; s++) {
+                if (arr[s] > arr[s + 1]) {
+                    int val = arr[s + 1];
+                    arr[s + 1] = arr[s];
+                    arr[s] = val;
+                }
+            }
+        }
+
     }
 
 }
