89c85d7d7cd5cf8d5b6f1ad2a9177ab5121b98f5
Valenstain <svs.laboratory@gmail.com>
2024-08-19 10:30:56 +0300

    Add empty classes: ArrayList and LinkedList

---
+++ src/collections/MyArrayList.java
@@ -0,0 +1,39 @@
+package collections;
+
+public class MyArrayList<T> implements MyList<T> {
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(int index) {
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(int index, T item) {
+
+    }
+
+    @Override
+    public MyList<T> subList(int start, int end) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
